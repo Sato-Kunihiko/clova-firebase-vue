fd983b002895baa9080bb93e078785685edaec58
Kunihiko-Sato <kunihiko.sato@linecorp.com>
2018-12-13 20:27:48 +0900

    Implemented auth func and sync firebase database

---
+++ src/components/Home.vue
@@ -0,0 +1,63 @@
+<template>
+
+	<v-data-table
+	    :headers="headers"
+	    :items="displayItems"
+	    class="elevation-1"
+	  >
+	
+		<template slot="items" slot-scope="props">
+			<td>{{ props.item.message }}</td>
+		</template>
+		      
+	  <template slot="no-data">
+	    <v-btn color="primary" @click="loadData">Reload</v-btn>
+	  </template>
+	  
+	</v-data-table>
+	  
+</template>
+
+<script>
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+export default {
+  data () {
+    return {
+      data: [],
+      headers: [
+        { text: 'message', value: 'message', sortable: false }
+      ]
+    }
+  },
+  computed: {
+    dataId () {
+      return this.$route.params.id
+    },
+    displayItems () {
+      return this.data
+    }
+  },
+  created () {
+    this.loadData()
+  },
+  methods: {
+    loadData () {
+      const ref = firebase.firestore().collection('items')
+      ref.onSnapshot(querySnapshot => {
+        this.data = []
+        querySnapshot.forEach(doc => {
+          const message = {
+            id: doc.id,
+            message: doc.data().message
+          }
+          this.data.push(message)
+        })
+      })
+    }
+  }
+}
+</script>
+
+
