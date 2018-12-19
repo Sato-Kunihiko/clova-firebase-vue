eb518e2c3cc38ede34c47efb181ddfbbc5e3b995
Kunihiko-Sato <kunihiko.sato@linecorp.com>
2018-12-19 09:30:53 +0900

    Set firebase config

---
+++ vue-client/src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import { store } from './store'
+import firebase from 'firebase'
+
+Vue.use(Vuetify)
+/* Write your firebase setting */
+firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID
+})
+const settings = {timestampsInSnapshots: true}
+firebase.firestore().settings(settings)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+const unsubscribe = firebase.auth()
+.onAuthStateChanged((firebaseUser) => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+    created () {
+      if (firebaseUser) {
+        store.dispatch('autoSignIn', firebaseUser)
+      }
+    }
+  })
+  unsubscribe()
+})
